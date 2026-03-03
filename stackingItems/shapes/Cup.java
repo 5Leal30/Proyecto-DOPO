f64e0344149e9eee0567fad80b25072c1054b06d
5Leal30 <sebastianleal583@gmail.com>
2026-03-02 21:49:56 -0500

    clase Cup

--- stackingItems/shapes/Cup.java
+++ stackingItems/shapes/Cup.java
@@ -7,11 +7,10 @@
  */
 public class Cup
 {
-    // instance variables - replace the example below with your own
     private int cupHeight;
     private Lid ofLid;
     private String colorCup;
-    private Rectangle[] cupRectangles = new Rectangle[3];
+    private Rectangle[] cupRectangles;
     private int xPositionCup;
     private int yPositionCup;
     private int cupNumber;
@@ -22,59 +21,103 @@ public class Cup
     /**
      * Constructor for objects of class Cup
      */
-    public void Cup(int cupHeight, int cupWidth)
+    public Cup(int cupNumber, int cupWidth)
     {
-        // initialise instance variables
+        this.cupNumber = cupNumber;
+        this.cupHeight = cupNumber;
+        this.cupWidth = cupWidth;
+        this.ofLid = null;
+        this.colorCup = assignColor(cupNumber);
+        this.xPositionCup = 0;
+        this.yPositionCup = 0;
+        this.cupRectangles = new Rectangle[3];
+        construirTaza();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
+    private String assignColor(int number) {
+        String[] colors = {"yellow","blue","red","green","green","orange"};
+        return colors[(number-1) % colors.length];
+    }
+    
+    private void construirTaza(){
+        int grosor = 3;
+        cupRectangles[0] = new Rectangle();
+        cupRectangles[0].changeSize(cupHeight, grosor);
+        cupRectangles[0].changeColor(colorCup);
+        
+        cupRectangles[1] = new Rectangle();
+        cupRectangles[1].changeSize(cupHeight, grosor);
+        cupRectangles[1].changeColor(colorCup);
+        
+        cupRectangles[2] = new Rectangle();
+        cupRectangles[2].changeSize(grosor, cupWidth);
+        cupRectangles[2].changeColor(colorCup);
+        
+    }
+    
+    public void setPosition(int x, int y){
+        this.xPositionCup = x;
+        this.yPositionCup = y;
+        int width = cupWidth;
+        int grosor = 3;
+        
+        cupRectangles[0].moveHorizontal(x - cupRectangles[0].getXPosition());
+        cupRectangles[0].moveVertical(y - cupRectangles[0].getYPosition());
+        cupRectangles[1].moveHorizontal((x + width - grosor) - cupRectangles[1].getXPosition());
+        cupRectangles[1].moveVertical(y - cupRectangles[1].getYPosition());
+        cupRectangles[2].moveHorizontal(x - cupRectangles[2].getXPosition());
+        cupRectangles[2].moveVertical((y + cupHeight - grosor) - cupRectangles[2].getYPosition());
+    }
+    
     public boolean hasLid()
     {
-        return 
+        return ofLid != null;
     }
     
     public int getCupHeight()
     {
-        
+        return cupHeight;
     }
     
     public int getCupNumber()
     {
-        
+        return cupNumber;
     }
     
     public String getCupColor()
     {
-        
+        return colorCup;
     }
     
     public void setCupColor(String color)
     {
-    
+        this.colorCup = color;
+        for (Rectangle r : cupRectangles){
+            r.changeColor(color);
+        }
     }
     
     public void makeVisible()
     {
-        
+        for (Rectangle r: cupRectangles){
+            r.makeVisible();
+        }
     }
     
     public void makeInvisible()
     {
-        
+        for (Rectangle r: cupRectangles){
+            r.makeInvisible();
+        }
     }
     
     public void setLid(Lid lid)
     {
-        
+        this.ofLid = lid;
     }   
     
-    
-    
-    
+    public Lid getLid(){
+        return ofLid;
+    }
     
 }
